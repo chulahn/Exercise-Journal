4b23c11264000db5da5f69fc615441095b05e78a
Chul Ahn <scahn.x@gmail.com>
2018-05-01 19:30:01 -0400

    setup mongo connection

--- controller.js
+++ controller.js
@@ -3,6 +3,11 @@ angular.module('app', [])
 
 .controller('dataController', ['$scope' , '$http' , function($scope, $http) {
 
+    $http.get("/ex")
+        .success(function(data) {
+            console.log(data);
+        })
+
     $scope.workout = [];
     $scope.exercises = [
         {name: "Press", weight: 135, rep: 5, time: new Date('4/28/16 18:16') },
@@ -11,8 +16,7 @@ angular.module('app', [])
 
     ];
 
-    $scope.addExercise = function(data) {
-        data = 0;
+    $scope.addExercise = function() {
         var newExercise = {};
         newExercise.name = $scope.exerciseName;
         newExercise.weight = $scope.exerciseWeight || 0;
@@ -21,5 +25,14 @@ angular.module('app', [])
         console.log(newExercise)
         $scope.exercises.push(newExercise);
     }
-    
+    $scope.logData = function() {
+        console.log($scope.exercises);
+
+        var text = JSON.stringify($scope.exercises);
+        console.log(text);
+
+        console.log($.parseJSON(text));
+    }
+
+
 }]);
--- index.js
+++ index.js
@@ -1,12 +1,46 @@
 var express = require("express");
 var app = express();
 
+var mongo = require("mongodb");
+var MongoClient = mongo.MongoClient;
+var databaseURL = "mongodb://admin:Fitadmin@ds121696.mlab.com:21696/exercise-journal"
+
 app.get("/", function(req, res) {
 	res.sendfile("index.html");
 });
 
 app.get("/controller.js", function(req, res) {
 	res.sendfile("controller.js");
+});
+
+app.get("/ex", function(req, res){ 
+    MongoClient.connect(databaseURL, function(err, client) {
+
+        if (client) {
+            console.log("Connected to client");
+            console.log(client);
+            var db = client.db('exercise-journal');
+
+            console.log("Database");
+            console.log(db);
+
+            var workoutCollection = db.collection("workouts");
+            console.log("Collection");
+            console.log(workoutCollection);
+
+            workoutCollection.find({}).toArray(function(err, results) {
+                if (results) {
+                    console.log(results);
+                }
+            });
+        }
+
+        else {
+            console.log("Error connecting to Database");
+            console.log(err);
+        }
+
+    });
 });
 
 
